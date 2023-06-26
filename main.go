56c24fd836f18d0544b6b0c500e5cf65368f239d
SHerlihy <steven_herlihy@yahoo.com>
2023-06-26 12:04:44 +0100

    find tilt in binary tree

--- main.go
+++ main.go
@@ -1,32 +1,51 @@
 package main
 
-import (
-    "math"
-)
-
  type TreeNode struct {
      Val int
      Left *TreeNode
      Right *TreeNode
  }
+func findTilt(root *TreeNode) int {
+    tiltSum := 0
 
-func findTilt(root *TreeNode){
+    if root == nil {
+        return tiltSum
+    }
+
+    evalBranch(root, &tiltSum)
+
+    return tiltSum
+}
+
+func evalBranch(branch *TreeNode, tiltSum *int) int {
     leftVal := 0
     rightVal := 0
 
-    if root.Left != nil {
-        leftVal = root.Left.Val
-        leftVal = leftVal + findTilt(root.Left)
+    if branch.Left != nil {
+        leftVal = branch.Left.Val
+
+        evalLeft = evalBranch(branch.Left, tiltSum)
+
+        leftVal = leftVal + evalLeft
     }
 
-    if root.Right != nil {
-        rightVal = root.Right.Val
-        rightVal = rightVal + findTilt(root.Right)
+    if branch.Right != nil {
+        rightVal = branch.Right.Val
+        evalRight = evalBranch(branch.Right, tiltSum)
+
+        rightVal = rightVal + evalRight
     }
+
+    tilt := 0
 
     if leftVal < rightVal {
-        return rightVal - leftVal
+        tilt = rightVal - leftVal
     } else {
-        return leftVal - rightVal
+        tilt =  leftVal - rightVal
     }
+
+    tiltSum = tiltSum + tilt
+
+    return leftVal + rightVal
 }
+
